7c092a42b468cc709261212698d7e4bbb02c25d4
Nadeem <110535104+nadeem-cs@users.noreply.github.com>
2024-09-03 16:45:48 +0530

    Revert "fix: replaced default export to named export"

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import contentstack from './lib/contentstack';
+import * as contentstack from './lib/contentstack';
 export * from './lib/types';
 
-export { contentstack as stack };
+export default contentstack;
