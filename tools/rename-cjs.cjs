550deb74db832d7b5927ba082a3381bd9ff5dc65
raj pandey <raj.pandey@contentstack.com>
2024-11-15 16:23:24 +0530

    fix: Resolved semgrep path traversal issue

--- tools/rename-cjs.cjs
+++ tools/rename-cjs.cjs
@@ -1,17 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
+const sanitizePath = (str) => str?.replace(/^(\.\.(\/|\\|$))+/, '');
+
 function renameJsToCjs(dir) {
   const files = fs.readdirSync(dir);
 
   files.forEach((file) => {
-    const filePath = path.join(dir, file);
+    const filePath = path.join(sanitizePath(dir), sanitizePath(file));
     const stat = fs.statSync(filePath);
 
     if (stat.isDirectory()) {
       renameJsToCjs(filePath);
     } else if (path.extname(file) === ".js") {
-      const newPath = path.join(dir, `${path.basename(file, ".js")}.cjs`);
+      const newPath = path.join(sanitizePath(dir), `${path.basename(sanitizePath(file), ".js")}.cjs`);
       fs.renameSync(filePath, newPath);
     }
   });
