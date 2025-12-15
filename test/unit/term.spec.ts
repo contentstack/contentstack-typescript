13fa7d9c25d2cca9c49cabedff54d897d3a1811c
harshitha.d <harshitha.d@contentstack.com>
2025-12-15 13:12:33 +0530

    refactor: update API endpoints from /taxonomy-manager to /taxonomies

--- test/unit/term.spec.ts
+++ test/unit/term.spec.ts
@@ -20,28 +20,28 @@ describe('Term class', () => {
   });
 
   it('should fetch the term by uid response when fetch method is called', async () => {
-    mockClient.onGet('/taxonomy-manager/taxonomy_testing/terms/term1').reply(200, termQueryFindResponseDataMock.terms[0]); //TODO: change to /taxonomies
+    mockClient.onGet('/taxonomies/taxonomy_testing/terms/term1').reply(200, termQueryFindResponseDataMock.terms[0]);
 
     const response = await term.fetch();
     expect(response).toEqual(termQueryFindResponseDataMock.terms[0]);
   });
 
   it('should fetch locales for a term when locales() is called', async () => {
-    mockClient.onGet('/taxonomy-manager/taxonomy_testing/terms/term1/locales').reply(200, termLocalesResponseDataMock.terms); //TODO: change to /taxonomies
+    mockClient.onGet('/taxonomies/taxonomy_testing/terms/term1/locales').reply(200, termLocalesResponseDataMock.terms);
 
     const response = await term.locales();
     expect(response).toEqual(termLocalesResponseDataMock.terms);
   });
 
   it('should fetch ancestors for a term when ancestors() is called', async () => {
-    mockClient.onGet('/taxonomy-manager/taxonomy_testing/terms/term1/ancestors').reply(200, termAncestorsResponseDataMock);
+    mockClient.onGet('/taxonomies/taxonomy_testing/terms/term1/ancestors').reply(200, termAncestorsResponseDataMock);
 
     const response = await term.ancestors();
     expect(response).toEqual(termAncestorsResponseDataMock);
   });
 
   it('should fetch descendants for a term when descendants() is called', async () => {
-    mockClient.onGet('/taxonomy-manager/taxonomy_testing/terms/term1/descendants').reply(200, termDescendantsResponseDataMock);
+    mockClient.onGet('/taxonomies/taxonomy_testing/terms/term1/descendants').reply(200, termDescendantsResponseDataMock);
 
     const response = await term.descendants();
     expect(response).toEqual(termDescendantsResponseDataMock);
